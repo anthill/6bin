7480e8b321932ada1de0454ae7e83473b2ee4e5b
Romain Crestey <rcrestey@gmail.com>
2016-01-06 15:46:23 +0100

    updated example.js with initial bins

--- example.js
+++ example.js
@@ -7,8 +7,21 @@ var server = new BinServer();
 
 server.start();
 
-// var initialBins = [{"id":"Cartons_1","p":15,"a":true,"t":"Cartons"}];
-var initialBins = [];
+var initialBins = [
+    {"id":"Cartons_1","p":15,"a":true,"t":"Cartons"},
+    {"id":"Cartons_2","p":15,"a":true,"t":"Cartons"},
+    {"id":"Cartons_3","p":15,"a":true,"t":"Cartons"},
+    {"id":"Cartons_4","p":15,"a":true,"t":"Cartons"},
+    {"id":"Cartons_5","p":15,"a":true,"t":"Cartons"},
+    {"id":"Cartons_6","p":15,"a":true,"t":"Cartons"},
+    {"id":"Cartons_7","p":15,"a":true,"t":"Cartons"},
+    {"id":"Cartons_8","p":15,"a":true,"t":"Cartons"},
+    {"id":"Cartons_9","p":15,"a":true,"t":"Cartons"},
+    {"id":"Cartons_10","p":15,"a":true,"t":"Cartons"},
+    {"id":"Cartons_11","p":15,"a":true,"t":"Cartons"},
+    {"id":"Cartons_12","p":15,"a":true,"t":"Cartons"}
+];
+// var initialBins = [];
 
 server.on('measurementRequest', function(request){
     var self = this;
@@ -48,7 +61,7 @@ server.on('getBinsRequest', function(request){
             index: request.index,
             isSuccessful: true,
             data: {
-                owner: 'Sinoe',
+                owner: 'Agglo_Pau',
                 bins: initialBins
             } // comment this to make the app crash on init
         });
